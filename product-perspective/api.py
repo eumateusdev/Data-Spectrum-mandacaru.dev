0227846697331a20860f30034144c00ef8fad219
Deusdedit99 <64427453+Deusdedit99@users.noreply.github.com>
2023-12-08 13:05:55 -0300

    Add files via upload

---
+++ product-perspective/api.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+model = joblib.load('modelo_treinado.joblib')
+vectorizer = joblib.load('vectorizer.joblib')
+
+app = Flask(__name__)
+
+@app.route('/classificar', methods=['POST'])
+def classificar():
+    # Obter os dados da requisição
+    data = request.get_json()
+
+    # Extrair o texto a ser classificado da requisição
+    texto = data['texto']
+
+    # Vetorizar o texto usando o vetorizador treinado
+    texto_vetorizado = vectorizer.transform([texto])
+
+    # Fazer a predição usando o modelo treinado
+    resultado = model.predict(texto_vetorizado)[0]
+
+    # Retornar o resultado como JSON
+    return jsonify({'sentimento': resultado})
+
+if __name__ == '__main__':
+    app.run(debug=True)
