311875378446895557a65c1e79c2626e82fec946
Deusdedit99 <64427453+Deusdedit99@users.noreply.github.com>
2023-12-08 13:10:47 -0300

    Update api.py

--- product-perspective/api.py
+++ product-perspective/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-model = joblib.load('modelo_treinado.joblib')
+model = joblib.load('product-perspective/modelo_treinado.joblib')
 vectorizer = joblib.load('vectorizer.joblib')
 
 app = Flask(__name__)
