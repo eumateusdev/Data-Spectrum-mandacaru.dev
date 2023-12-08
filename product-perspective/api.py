48df9b98b8bc6e410d1714556d15d2295193902e
Deusdedit99 <64427453+Deusdedit99@users.noreply.github.com>
2023-12-08 15:53:03 -0300

    Update api.py

--- product-perspective/api.py
+++ product-perspective/api.py
@@ -1,13 +1,14 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-model = joblib.load('product-perspective/modelo_treinado.joblib')
-vectorizer = joblib.load('product-perspective/vectorizer.joblib')
+model = joblib.load('modelo_treinado.joblib')
+vectorizer = joblib.load('vectorizer.joblib')
 
 app = Flask(__name__)
-
-@app.route('/classificar', methods=['POST'])
+CORS(app) 
+@app.route('/', methods=['POST'])
 def classificar():
     # Obter os dados da requisição
     data = request.get_json()
